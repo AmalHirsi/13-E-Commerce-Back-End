280007949c314d40278814e1f4eb2788c5df50ef
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-12-04 08:49:42 +0000

    working on PUT

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,8 +39,19 @@ router.post('/', async (req, res) => {
 
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
-});
+    try {
+      const CategoryData = await Category.update({
+        category_name: req.body.category_name,
+      },
+        where: {
+          id: req.params.id
+        },
+      );
+      res.status(200).json(CategoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.delete('/:id', async (req, res) => {
   try {
@@ -49,7 +60,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id
       }
     });
-    res.status(200).json(locationData);
+    res.status(200).json(CategoryData);
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,21 +8,54 @@ router.get('/', (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+outer.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll()({
+      include:[{model: Product, through: ProductTag, as: "tagged_product"}],
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.get('/:id', async (req, res) => {
+  try {
+    const TagData = await Tag.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include:[{model: Product, through: ProductTag, as: "tagged_product"}],
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+    try {
+      const TagData = await Category.update({
+        tag_name: req.body.tag_name,
+        where: {
+          id: req.params.id
+        },
+      });
+      res.status(200).json(CategoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const TagData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
